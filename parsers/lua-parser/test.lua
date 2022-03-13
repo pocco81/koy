bcedb9d5d5512e4eda70023b29df56125dd79275
Pocco81 <pocco451@gmail.com>
2022-03-13 16:17:59 -0500

    fix: formatting and cleaned comments

--- parsers/lua-parser/test.lua
+++ parsers/lua-parser/test.lua
@@ -3,28 +3,16 @@ local inspect = require("deps.inspect")
 
 local koy_sample = require("src.utils").dump_file("./samples/s1.koy")
 local parsed_koy = koy_parser.decode(koy_sample)
-print(inspect(parsed_koy))
 
--- local hello = world
--- local key = "ayy $1  ${hey} i'm a ${ffd} ${a} key. ${hello} lol [hello]"
---
--- local test_key = "The fat cat sat on the mat."
---
--- -- local regex = [[${.*}]]
--- print( key:find("[\\$][\\{].*[\\}]") )
---
---
--- -- print(key)
--- -- print(key:find("\\[(.*?)\\]"))
--- -- print(key:find("[\\$]")) -- matches $
--- -- print(key:find("([\\$][\\{])")) -- matches ${
--- -- print(key:find("([\\$][\\{]).+([\\}])")) -- matches ${ whatever in here }
--- -- print(key:find("([\\$][\\{]).([\\}])")) -- matches ${a} only one char
---
--- -- print(key:find("[\\{](.*?)[\\}]"))
---
--- -- print(key:find("([\\$][\\{])([\\}])")) -- matches ${a} only one char
---
--- -- print(key:sub(18,18))
---
--- -- print(test_key:match("(f|c|m)at\.?"))
+-- NOTE: when using inspector, tables that have the same content will be represented by pointers:
+--[[ for example:
+{
+  person1 = <1>{
+    age = 121,
+    name = "Michael"
+  },
+  person2 = <table 1>
+}
+--]]
+
+print(inspect(parsed_koy))
